3623f9399a01c4a37581e7ab19b3b51dd51ef279
Yandé Guissé <yandeiguiss2@gmail.com>
2025-11-11 01:39:44 +0000

    modeles et mogrations

---
+++ database/migrations/2025_11_11_012232_create_hotels_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hotels', function (Blueprint $table) {
+            $table->id();
+            $table->string('nomHotel');
+            $table->string('addresse');
+            $table->string('prixNuitee');
+            $table->string('numero');
+            $table->string('email')->unique();
+             $table->enum('devise', ['F CFA', 'Euro', 'Dollar']);
+            $table->string('cheminImage');            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hotels');
+    }
+};
